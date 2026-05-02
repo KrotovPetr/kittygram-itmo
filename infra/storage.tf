fa6e6704c8f5453570128d6c0a55bcd788b73af8
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2026-05-02 21:26:31 +0300

    change terraform files

---
+++ infra/storage.tf
@@ -0,0 +1,17 @@
+resource "yandex_storage_bucket" "tf_state" {
+  bucket = "itmo-test"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    id      = "retain-all"
+    enabled = true
+    noncurrent_version_expiration {
+      days = 180
+    }
+  }
+
+  force_destroy = false
+}
