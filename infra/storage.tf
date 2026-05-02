1099ee66a8ef964485f1b189bcdff43fd6faee24
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2026-05-02 21:04:57 +0300

    change terraform files

--- infra/storage.tf
+++ infra/storage.tf
@@ -15,8 +15,3 @@ resource "yandex_storage_bucket" "tf_state" {
 
   force_destroy = false
 }
-
-resource "yandex_storage_bucket_acl" "tf_state_acl" {
-  bucket = yandex_storage_bucket.tf_state.bucket
-  acl    = "private"
-}
